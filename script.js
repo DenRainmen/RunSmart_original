92f6453eb16a060ef9aaf6351bf1edcc71079db9
DenRainmen <64159411+DenRainmen@users.noreply.github.com>
2024-07-08 18:25:48 +0300

    Добавил скрипты для переворота карточек

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Карточки для фитнеса
 
-let btn = document.querySelector(".details");
+/* let btn = document.querySelector(".details");
 
 let card = document.querySelector(".front-side");
 
@@ -19,4 +19,56 @@ btn.addEventListener("click", function(){
 btnBack.addEventListener("click", function(){
   card.classList.toggle("active");
   productCardBack.classList.toggle("active");
-})
+}) */
+
+
+  let frontCollection = document.querySelectorAll('.front-side');
+
+  let backCollection = document.querySelectorAll('.back-side');
+
+
+
+function frontSideFlip(event) {
+
+  
+   /* console.log(frontCollection); */
+
+
+   frontCollection.forEach((el, index) => {
+
+    el.addEventListener('click', () => {
+
+      event.currentTarget.classList.toggle("active")
+ 
+     backCollection[index].classList.add("active")
+
+    });
+
+   
+
+  })
+
+
+
+
+} ;
+
+
+function backSideFlip(event) {
+
+  let blockCollection = document.querySelectorAll('.back-side');
+
+  
+  blockCollection.forEach((el, index) => {
+
+    el.addEventListener('click', () => {
+
+      backCollection[index].classList.remove('active');
+      frontCollection[index].classList.remove('active')
+
+    });
+
+   
+  })
+
+} 
